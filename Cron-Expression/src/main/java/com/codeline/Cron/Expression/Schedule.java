71ac7e987cf1a4514a5b458b31f404cc8b28d513
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-06-01 16:39:22 +0400

    Problem 20

--- Cron-Expression/src/main/java/com/codeline/Cron/Expression/Schedule.java
+++ Cron-Expression/src/main/java/com/codeline/Cron/Expression/Schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    @Scheduled(cron = "0 * */2 * *")
+    @Scheduled(cron = "*/5 9-17 * * 6-7")
     public void schedle1(){
         System.out.println("Hello world");
     }
